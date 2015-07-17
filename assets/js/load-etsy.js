102ec2d3043fb0f44d706f0d64bc1db47679427d
Corbin Otwell <cbotwell@gmail.com>
2015-07-17 15:17:00 -0500

    got items styled, working on header

--- assets/js/load-etsy.js
+++ assets/js/load-etsy.js
@@ -17,4 +17,10 @@ var loadEtsy = function (query, onload, immediate) {
     if (immediate) {
     immediate();
     }
+
+    function htmlDecode(input){
+      var e = document.createElement('div');
+      e.innerHTML = input;
+      return e.childNodes.length === 0 ? "" : e.childNodes[0].nodeValue;
+    }
 };
